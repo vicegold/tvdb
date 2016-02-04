888dc9d01838c1d2793eed671ff02863504afc3a
laurids <laurids@vicegold.com>
2016-02-04 22:00:06 +0100

    fix home intro text

--- build/header.php
+++ build/header.php
@@ -21,7 +21,7 @@ if(isset($_GET['page'])) {
         <div class="tv-wrapper">
           <svg class="icon"><use xlink:href="/assets/svgsprites/symbol/svg/sprite.symbol.svg#TheTVDB" /></svg>
           <span>An open database for television</span>
-          <?php if($page == "home") : ?>
+          <?php if($page != "show") : ?>
             <p>Welcome to TheTVDB.com. This site is an open database that can be modified by anybody. Please click on login in the navigation to get an account. All content and images on the site have been contributed by our users, and are licensed under a <a href="#">Creative Commons Attribution-NonCommercial 4.0 International License</a>.</p>
             <p>This site also has a <a href="#">full XML API</a> that allows other software and websites to use this information. The API is currently being used by the <a href="#">myTV add-in for Windows Media Center</a>, <a href="#">Kodi</a>, <a href="#">the meeTVshows and TVNight plugins</a> for <a href="#">Meedio</a>, the MP-TVSeries plugin for MediaPortal, Numote (iPhone/Android app and set-top device), and many more.</p>
             <p>If you find this site useful, please consider <a href="#">donating</a>.</p>
