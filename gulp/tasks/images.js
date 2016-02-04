06cd06dcba0c5bbf92fdb32b6690e8a5c2b89853
laurids <laurids@vicegold.com>
2016-02-04 21:56:01 +0100

    remove image compression on production

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -4,7 +4,7 @@
 gulp.task('images', function() {
     return gulp.src(bower(build.files.images).concat(build.files.images.map(function (file) { return paths.images.src + '/' + file; })))
         .pipe($.newer(paths.images.dest))
-        .pipe(isProduction ? $.imagemin({ optimizationLevel: 7, progressive: true, interlaced: true }) : _.noop())
+        // .pipe(isProduction ? $.imagemin({ optimizationLevel: 7, progressive: true, interlaced: true }) : _.noop())
         .pipe(gulp.dest(paths.images.dest))
         .pipe($.size({ title: 'images done.' }))
         .pipe(browserSync.reload({ stream: true }));
