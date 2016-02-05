1135eb1eca9948767787f488ba09ae53d0b04238
laurids <laurids@vicegold.com>
2016-02-05 02:19:22 +0100

    menu scroll fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,12 @@
 $( document ).ready(function() {
+  var headerHeight = $('.tv-header--intro').height();
+  console.log(headerHeight);
+
   $(window).on('scroll', function() {
     $('.show #nav').toggleClass('fixed', $(this).scrollTop() > 156);
-    $('.home #nav').toggleClass('fixed', $(this).scrollTop() > 556);
+    $('.home #nav, .tv-header').toggleClass('fixed', $(this).scrollTop() > headerHeight + 70);
     $('#nav').toggleClass('mobilescroll', $(this).scrollTop() > 59);
   });
 
-  // var headerHeight = $('.tv-header--intro').height();
-  // $('.tv-header--background').css('height', headerHeight);
+
 });
